c1ee134d23925fd112b8a21c12e9ae1b3bbebe88
Swapneel Sarkar <sarkarswapneel14@gmail.com>
2026-05-31 13:01:29 +0530

    Home Layout and SideBar created

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -1,11 +1,80 @@
 import React from "react";
+import { useState } from "react";
+import SideBar from "../components/home-componets/SideBar";
+import AllPosts from "../components/home-componets/AllPosts";
+import Profile from "../components/home-componets/Profile";
 
-function Home(){
-    return(
-        <>
-         <h1>Home Page!!!</h1>
-        </>
-    )
+import { Routes, Route } from "react-router-dom";
+import Messages from "../components/home-componets/Messages";
+import LostForm from "../components/home-componets/LostForm";
+import FoundForm from "../components/home-componets/FoundForm";
+
+function Home() {
+    const [showSidebar, setShowSidebar] = useState(false);
+
+    return (
+
+        <div className="container-fluid">
+            <button
+                className="mobile-menu-btn"
+                onClick={() => setShowSidebar(!showSidebar)}
+            >
+                ☰
+            </button>
+
+            <div className="row g-0">
+
+                 <div className={`col-lg-2 sidebar-wrapper ${
+            showSidebar ? "show" : ""
+        }`}>
+
+            <SideBar/>
+
+        </div>
+                <div className="col-lg-10 col-md-9">
+
+                    <Routes>
+
+                        <Route
+                            index
+                            element={<AllPosts />}
+                        />
+
+
+                        <Route
+                            path="profile"
+                            element={<Profile />}
+                        />
+
+                        <Route
+                            path="messages"
+                            element={<Messages />}
+                        />
+
+                        <Route
+                            path="messages"
+                            element={<Messages />}
+                        />
+
+                        <Route
+                         path="report-lost"
+                         element={<LostForm/>}
+                        />
+                        <Route
+                         path="report-found"
+                         element={<FoundForm/>}
+                        />
+
+
+                    </Routes>
+
+                </div>
+
+            </div>
+
+        </div>
+
+    );
 }
 
-export default  Home;
+export default Home;
