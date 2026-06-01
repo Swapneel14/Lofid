fb2df2dd0f4833796e5f117a04d436a27b5b4de7
Swapneel Sarkar <sarkarswapneel14@gmail.com>
2026-06-01 23:12:36 +0530

    inngest and clerk connected

---
+++ backend/inngest/index.js
@@ -0,0 +1,48 @@
+import { Inngest } from "inngest";
+
+export const inngest = new Inngest({ id: "LoFid" });
+
+//to save user data to a data base
+
+const syncUserCreation = inngest.createFunction(
+    {
+    id: "sync-user-from-clerk",
+    triggers: [{ event: "clerk/user.created" }],
+  },
+    async({event})=>{
+         const {id,first_name,last_name,email_addresses,image_url}=event.data;
+
+         const userData = {
+            _id:id,
+            email:email_addresses[0].email_address,
+            name:first_name+' '+last_name,
+            image:image_url
+         }
+         console.log(userData);
+    }
+
+)
+
+//delete user from database
+
+const syncUserDeletion = inngest.createFunction(
+    {
+    id: "delete-user-with-clerk",
+    triggers: [{ event: "clerk/user.deleted" }],
+  },
+    async({event})=>{
+         const {id,first_name,last_name,email_addresses,image_url}=event.data;
+
+         const userData = {
+            _id:id,
+            email:email_addresses[0].email_address,
+            name:first_name+' '+last_name,
+            image:image_url
+         }
+         console.log(userData);
+    }
+
+)
+
+
+export const functions = [syncUserCreation];
--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,10 @@ import cors from "cors"
 import mongoose from "mongoose";
 import { connectDB } from "./config/db.js";
 
+import { clerkMiddleware } from '@clerk/express'
+import { functions, inngest } from "./inngest/index.js";
+import {serve} from 'inngest/express';
+
 const app = express();
 const port = 6769;
 
@@ -12,10 +16,13 @@ connectDB();
 
 app.use(express.json());
 app.use(cors())
+app.use(clerkMiddleware())
 
 app.get("/", (req, res) => {
     res.send("API working")
 })
+
+app.use('/api/inngest',serve({client:inngest,functions}))
 
 app.listen(port, () => {
     console.log(`Server started at http://localhost:${port}`)
