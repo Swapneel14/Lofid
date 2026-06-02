ca3d0932b7a2b26a041f1a8f92d45ce22f196659
Udayx404 <udaygit88@gmail.com>
2026-06-02 13:11:53 +0530

    LostItem schema created

---
+++ backend/models/LostItem.js
@@ -0,0 +1,77 @@
+import mongoose from "mongoose";
+
+const lostItemSchema = new mongoose.Schema(
+  {
+    user: {
+      type: String,
+      ref: "User", // references User_id (Clerk user id)
+      required: true,
+    },
+
+    itemName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    category: {
+      type: String,
+      required: true,
+      enum: [
+        "Electronics",
+        "Clothing & Accessories",
+        "Books & Stationery",
+        "ID & Documents",
+        "Keys",
+        "Bags & Backpacks",
+        "Water Bottles & Tiffin",
+        "Jewellery & Watches",
+        "Sports Equipment",
+        "Wallet & Purse",
+        "Other",
+      ],
+    },
+
+    images: [
+      {
+        type: String, // image URLs
+      },
+    ],
+
+    description: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    lostDate: {
+      type: Date,
+      required: true,
+    },
+
+    lostTime: {
+      type: String,
+      required: true,
+    },
+
+    lostLocation: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    status: {
+      type: String,
+      enum: ["Lost", "Found", "Claimed"],
+      default: "Lost",
+    },
+  },
+  {
+    timestamps: true,
+    minimize: false
+  },
+);
+
+const LostItem = mongoose.models.LostItem || mongoose.model("LostItem", lostItemSchema);
+
+export default LostItem;
