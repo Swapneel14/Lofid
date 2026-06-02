da9e606daa74a68d789cbe877c65d91cbeb6df1e
Swapneel Sarkar <sarkarswapneel14@gmail.com>
2026-06-02 08:02:26 +0530

    User Deletion Function Fixed

--- backend/inngest/index.js
+++ backend/inngest/index.js
@@ -36,23 +36,32 @@ const syncUserCreation = inngest.createFunction(
 //delete user from database
 
 const syncUserDeletion = inngest.createFunction(
-    {
+  {
     id: "delete-user-with-clerk",
     triggers: [{ event: "clerk/user.deleted" }],
   },
-    async({event})=>{
-         const {id,first_name,last_name,email_addresses,image_url}=event.data;
+  async ({ event }) => {
+    try {
+      const {
+        id,
+        first_name,
+        last_name,
+        email_addresses,
+        image_url,
+      } = event.data;
 
-         const userData = {
-            _id:id,
-            email:email_addresses[0].email_address,
-            name:first_name+' '+last_name,
-            image:image_url
-         }
-         console.log(userData);
+      const userData = {
+        _id: id,
+        email: email_addresses?.[0]?.email_address,
+        name: `${first_name || ""} ${last_name || ""}`.trim(),
+        image: image_url,
+      };
+
+      console.log(userData);
+    } catch (err) {
+      console.error(err);
     }
-
-)
-
+  }
+);
 
 export const functions = [syncUserCreation,syncUserDeletion];
